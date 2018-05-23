ef05bc22b40126ee369ad019f354747d3b3dc50a
Conor Hamilton <conhamil19@gmail.com>
2018-05-23 07:47:28 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,2 +1,21 @@
 class Author 
+  attr_accessor :name :posts
+  
+  def initialize(name)
+    @name = name 
+    @posts = []
+  end
+  
+  def add_post(post)
+    post.author = self
+    @post << post
+  end 
+  
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+    @posts << post
+  end 
+  
+  
 end 
